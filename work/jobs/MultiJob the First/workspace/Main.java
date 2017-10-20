84f46c8afbd5191bd0dcd7eb56b37eb41b1db0fc
Rickard Bernal <rickard.bernal@avalanchestudios.se>
2017-10-20 18:35:11 +0200

    added the jobs so i dont have to rewrite them

---
+++ work/jobs/MultiJob the First/workspace/Main.java
@@ -0,0 +1,7 @@
+public class Main { 
+public static void main(String[] args){ 
+if (args.length > 0 && args[0].equalsIgnoreCase("true")) System.exit(1);  
+System.out.println("wawa I'm a string");  
+System.out.println("and I'm loving it"); 
+} 
+} 
